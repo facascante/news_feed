534264e85ee9615fbf5ec301efdae1682f72d226
chito <chito@burstsms.com>
2016-01-15 13:56:27 +0800

    added link for socket.io tutorial

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -3,12 +3,12 @@
     
     var app = angular.module('news-feeds',['ngResource']);
     app.controller('FeedsController', function($resource,$scope){
-        var socket = io();
-        $scope.news = [];
-        $resource('/api/news').query().$promise.then(function(data){
-            $scope.news = data;
-        });
-        
+        // var socket = io();
+        // $scope.news = [];
+        // $resource('/api/news').query().$promise.then(function(data){
+        //     $scope.news = data;
+        // });
+        //http://www.html5rocks.com/en/tutorials/frameworks/angular-websockets/
     })
     
 })();
